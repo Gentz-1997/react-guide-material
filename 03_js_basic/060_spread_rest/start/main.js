93951160374ac08c7115e504bf454cdc75dd8971
Genki Matsumi <genki.kuromu@gmail.com>
2024-12-31 17:33:48 +0900

    3-14,15を終了

--- 03_js_basic/060_spread_rest/start/main.js
+++ 03_js_basic/060_spread_rest/start/main.js
@@ -1,13 +1,14 @@
 const nums = [3, 1, 4, 1, 5, 10, 2, 6];
 
-const result = Math.max(3, 1, 4, 1, 5, 10, 2, 6);
+const result = Math.max(...nums);
 console.log(result);
 
 
-let arr1 = [1, 2, 3];
+let arr1 = [1, 2, 3, 20];
 let arr2 = [4, 5, 6];
-let newArr = [...arr1];
-
+let newArr = [...arr1, ...arr2, 10];
+let newArr2 = arr1
+console.log(newArr);
 console.log(newArr2);
 
 const obj = {
@@ -15,7 +16,9 @@ const obj = {
   age: 22,
 };
 const newObj = { ...obj };
+newObj.name = "John"
 
+console.log(obj);
 console.log(newObj);
 
 const restA = (...argA) => console.log(argA);
