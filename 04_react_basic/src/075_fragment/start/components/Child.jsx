32cfcd60e4b8225abe0d2cf8d26d4fdb14c51bdf
Genki Matsumi <genki.kuromu@gmail.com>
2025-01-07 23:17:25 +0900

    30を終了

--- 04_react_basic/src/075_fragment/start/components/Child.jsx
+++ 04_react_basic/src/075_fragment/start/components/Child.jsx
@@ -1,10 +1,15 @@
 import "./Child.css";
+import {Fragment} from "react";
 
 const Child = () => {
   return (
-    <div className="component">
+    <Fragment key="1">
+      <div className="component">
       <h3>Hello Component</h3>
-    </div>
+      </div>
+      <h3>Hello Fragment</h3>
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illum doloremque incidunt esse odit, atque obcaecati aut vero? Aliquid qui suscipit odit itaque, accusamus, nemo deleniti eveniet recusandae praesentium sequi numquam?</p>
+    </Fragment>
   );
 };
 
