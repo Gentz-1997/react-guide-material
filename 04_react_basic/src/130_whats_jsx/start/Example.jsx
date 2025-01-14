9fc841b6ba13acc8f9b979378c07ae4ab67fc1b1
Genki Matsumi <genki.kuromu@gmail.com>
2025-01-14 22:38:06 +0900

    39を終了

--- 04_react_basic/src/130_whats_jsx/start/Example.jsx
+++ 04_react_basic/src/130_whats_jsx/start/Example.jsx
@@ -2,7 +2,9 @@ import React from "react";
 
 const Example = () => {
   const sample1 = <h1 className="greeting">Hello World</h1>;
-
+  // console.log(React.createElement("h1", {
+  //   className: "greeting"
+  // }, "Hello World"));
   // React.createElement("h1", {
   //   className: "greeting"
   // }, "Hello World");
@@ -12,6 +14,11 @@ const Example = () => {
       <h1>Hello!</h1>
       <h2>Good to see you.</h2>
     </div>
+  );
+
+  React.createElement("div", null, 
+    React.createElement("h1", null, "Hello!"), 
+    React.createElement("h2", null, "Good to see you.")
   );
 
   console.log(
